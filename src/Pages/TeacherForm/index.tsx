193a05d6522b083b4b81f0b595388adcf7762041
vitor-risso <vitor.risso13@gmail.com>
2020-08-05 18:10:55 -0300

    style do teacher form e Input ok

--- src/Pages/TeacherForm/index.tsx
+++ src/Pages/TeacherForm/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg'
 
 import './styles.css'
 
@@ -10,8 +13,38 @@ function TeacherForm (){
         <PageHeader 
          title="Que incrível que você quer dar aula"
          description = "O primeiro passo é preecnher esse fomulário de inscrição"
-         
         />        
+
+        <main>
+            <fieldset>
+                <legend>Seus Dados</legend>
+
+                <Input name = "name" label = "Nome completo"/> 
+                <Input name = "avatar" label = "Avatar"/> 
+                <Input name = "whatsapp" label = "Whatasapp"/> 
+
+
+           
+            </fieldset>
+
+            <fieldset>
+                <legend>Sobre a aula</legend>
+
+                <Input name = "subject" label = "Matéria"/> 
+                <Input name = "cost" label = "Custo da sua hora aula"/> 
+           
+            </fieldset>
+
+            <footer>
+            <p> <img src={warningIcon} alt="Aviso importante"/> 
+            Importante! <br/>
+            Preencha todos os dados 
+            </p>
+            <button type="button">
+                Salvar cadastro
+            </button>
+            </footer>
+        </main>
 
      </div>
     )
